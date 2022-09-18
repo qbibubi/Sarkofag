744d9b9a199d95a262058fdda14271f734da0f1e
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2022-09-18 17:26:59 +0200

    captcha working

--- Sekretariat/Form1.cs
+++ Sekretariat/Form1.cs
@@ -5,6 +5,8 @@ namespace Sekretariat
 {
     public partial class Secretariat : Form
     {
+        public string captcha = "mxyxw";
+
         public Secretariat()
         {
             InitializeComponent();
@@ -19,24 +21,31 @@ namespace Sekretariat
             {
                 case 1:
                     CaptchaBox.Image = Properties.Resources._1;
+                    captcha = "mxyxw";
                     break;
                 case 2:
                     CaptchaBox.Image = Properties.Resources._2;
+                    captcha = "b5nmm";
                     break;
                 case 3:
                     CaptchaBox.Image = Properties.Resources._3;
+                    captcha = "74853";
                     break;
                 case 4:
                     CaptchaBox.Image = Properties.Resources._4;
+                    captcha = "cg5dd";
                     break;
                 case 5:
                     CaptchaBox.Image = Properties.Resources._5;
+                    captcha = "x3deb";
                     break;
                 case 6:
                     CaptchaBox.Image = Properties.Resources._6;
+                    captcha = "befhd";
                     break;
                 case 7:
                     CaptchaBox.Image = Properties.Resources._7;
+                    captcha = "c7gd3";
                     break;
             }
         }
@@ -46,24 +55,24 @@ namespace Sekretariat
             RandomImage();
         }
 
-        public string GetCurrentCaptcha()
-        {
-            string captcha = "";
-
-            return captcha;
-        }
-
         private void SignIn_Click(object sender, EventArgs e)
         {
             string log = "admin";
             string pwd = "Qwerty1@34";
-            string cap = GetCurrentCaptcha();
-
             bool login = (log == UserInput.Text && pwd == PasswordInput.Text);
-            bool matchCap = (cap == CaptchaCheck.Text);
+            bool matchCap = (captcha == CaptchaCheck.Text);
 
-
-
+            if (login && matchCap)
+            {
+                UserInput.BackColor = Color.Green;
+            } 
+            else
+            {
+                UserInput.Clear();
+                PasswordInput.Clear();
+                CaptchaCheck.Clear();
+                RandomImage();
+            }
         }
     }
 }
