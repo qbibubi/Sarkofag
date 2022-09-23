2ea9961fabe075b5d34966275927bf835de596e3
Qbi <visualstudent@zschie.edu.pl>
2022-09-23 13:58:17 +0200

    add user controls

---
+++ Sekretariat/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sekretariat
+{
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Name = "Login";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
