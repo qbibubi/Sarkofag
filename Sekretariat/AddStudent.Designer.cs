0bbb85508cbe571dc724b39c2d757cc1f202d7ec
Qbi <visualstudent@zschie.edu.pl>
2022-09-23 15:41:38 +0200

    XD

--- Sekretariat/AddStudent.Designer.cs
+++ Sekretariat/AddStudent.Designer.cs
@@ -28,19 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StudentTab = new System.Windows.Forms.TabControl();
+            this.SearchStudentTab = new System.Windows.Forms.TabPage();
+            this.AddStudentTab = new System.Windows.Forms.TabPage();
+            this.CriteriaLabel = new System.Windows.Forms.Label();
+            this.startsWith = new System.Windows.Forms.ComboBox();
+            this.studentName = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StudentTab.SuspendLayout();
+            this.SearchStudentTab.SuspendLayout();
+            this.AddStudentTab.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // StudentTab
+            // 
+            this.StudentTab.Controls.Add(this.SearchStudentTab);
+            this.StudentTab.Controls.Add(this.AddStudentTab);
+            this.StudentTab.Location = new System.Drawing.Point(3, 3);
+            this.StudentTab.Name = "StudentTab";
+            this.StudentTab.SelectedIndex = 0;
+            this.StudentTab.Size = new System.Drawing.Size(797, 444);
+            this.StudentTab.TabIndex = 0;
+            // 
+            // SearchStudentTab
+            // 
+            this.SearchStudentTab.Controls.Add(this.CriteriaLabel);
+            this.SearchStudentTab.Controls.Add(this.startsWith);
+            this.SearchStudentTab.Controls.Add(this.studentName);
+            this.SearchStudentTab.Controls.Add(this.button1);
+            this.SearchStudentTab.Controls.Add(this.richTextBox1);
+            this.SearchStudentTab.Controls.Add(this.textBox1);
+            this.SearchStudentTab.Location = new System.Drawing.Point(4, 24);
+            this.SearchStudentTab.Name = "SearchStudentTab";
+            this.SearchStudentTab.Padding = new System.Windows.Forms.Padding(3);
+            this.SearchStudentTab.Size = new System.Drawing.Size(789, 416);
+            this.SearchStudentTab.TabIndex = 0;
+            this.SearchStudentTab.Text = "Wyszukaj ucznia/uczniów";
+            this.SearchStudentTab.UseVisualStyleBackColor = true;
+            // 
+            // AddStudentTab
+            // 
+            this.AddStudentTab.Controls.Add(this.label3);
+            this.AddStudentTab.Controls.Add(this.label2);
+            this.AddStudentTab.Controls.Add(this.label1);
+            this.AddStudentTab.Controls.Add(this.textBox4);
+            this.AddStudentTab.Controls.Add(this.textBox3);
+            this.AddStudentTab.Controls.Add(this.textBox2);
+            this.AddStudentTab.Location = new System.Drawing.Point(4, 24);
+            this.AddStudentTab.Name = "AddStudentTab";
+            this.AddStudentTab.Padding = new System.Windows.Forms.Padding(3);
+            this.AddStudentTab.Size = new System.Drawing.Size(789, 416);
+            this.AddStudentTab.TabIndex = 1;
+            this.AddStudentTab.Text = "Dodaj ucznia";
+            this.AddStudentTab.UseVisualStyleBackColor = true;
+            // 
+            // CriteriaLabel
+            // 
+            this.CriteriaLabel.AutoSize = true;
+            this.CriteriaLabel.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CriteriaLabel.Location = new System.Drawing.Point(83, 13);
+            this.CriteriaLabel.Name = "CriteriaLabel";
+            this.CriteriaLabel.Size = new System.Drawing.Size(274, 25);
+            this.CriteriaLabel.TabIndex = 11;
+            this.CriteriaLabel.Text = "Wczytaj kryteria wyszukiwania";
+            // 
+            // startsWith
+            // 
+            this.startsWith.FormattingEnabled = true;
+            this.startsWith.Location = new System.Drawing.Point(314, 58);
+            this.startsWith.Name = "startsWith";
+            this.startsWith.Size = new System.Drawing.Size(153, 23);
+            this.startsWith.TabIndex = 10;
+            // 
+            // studentName
+            // 
+            this.studentName.FormattingEnabled = true;
+            this.studentName.Items.AddRange(new object[] {
+            "Imię",
+            "Nazwisko"});
+            this.studentName.Location = new System.Drawing.Point(83, 58);
+            this.studentName.Name = "studentName";
+            this.studentName.Size = new System.Drawing.Size(153, 23);
+            this.studentName.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(553, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Szukaj";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 142);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(777, 261);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(551, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 23);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 23);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(207, 23);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(286, 237);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(207, 23);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Imię";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
             // 
             // AddStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.StudentTab);
             this.Name = "AddStudent";
             this.Size = new System.Drawing.Size(800, 450);
+            this.StudentTab.ResumeLayout(false);
+            this.SearchStudentTab.ResumeLayout(false);
+            this.SearchStudentTab.PerformLayout();
+            this.AddStudentTab.ResumeLayout(false);
+            this.AddStudentTab.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private TabControl StudentTab;
+        private TabPage SearchStudentTab;
+        private TabPage AddStudentTab;
+        private Label CriteriaLabel;
+        private ComboBox startsWith;
+        private ComboBox studentName;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
     }
 }
--- Sekretariat/Login.cs
+++ Sekretariat/Login.cs
@@ -17,131 +17,7 @@ namespace Sekretariat
             InitializeComponent();
         }
 
-        private void InitializeComponent()
-        {
-            this.CaptchaCheck = new System.Windows.Forms.TextBox();
-            this.PasswordInput = new System.Windows.Forms.TextBox();
-            this.UserInput = new System.Windows.Forms.TextBox();
-            this.SignIn = new System.Windows.Forms.Button();
-            this.CaptchaBox = new System.Windows.Forms.PictureBox();
-            this.RandomCaptcha = new System.Windows.Forms.Label();
-            this.InstructionLabel = new System.Windows.Forms.Label();
-            this.VerificationLabel = new System.Windows.Forms.Label();
-            this.PasswordLabel = new System.Windows.Forms.Label();
-            this.UserLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.CaptchaBox)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // CaptchaCheck
-            // 
-            this.CaptchaCheck.Location = new System.Drawing.Point(317, 265);
-            this.CaptchaCheck.Name = "CaptchaCheck";
-            this.CaptchaCheck.Size = new System.Drawing.Size(285, 23);
-            this.CaptchaCheck.TabIndex = 19;
-            // 
-            // PasswordInput
-            // 
-            this.PasswordInput.Location = new System.Drawing.Point(317, 112);
-            this.PasswordInput.Name = "PasswordInput";
-            this.PasswordInput.PasswordChar = '*';
-            this.PasswordInput.Size = new System.Drawing.Size(285, 23);
-            this.PasswordInput.TabIndex = 18;
-            // 
-            // UserInput
-            // 
-            this.UserInput.Location = new System.Drawing.Point(317, 72);
-            this.UserInput.Name = "UserInput";
-            this.UserInput.Size = new System.Drawing.Size(285, 23);
-            this.UserInput.TabIndex = 17;
-            // 
-            // SignIn
-            // 
-            this.SignIn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.SignIn.Location = new System.Drawing.Point(493, 356);
-            this.SignIn.Name = "SignIn";
-            this.SignIn.Size = new System.Drawing.Size(109, 29);
-            this.SignIn.TabIndex = 16;
-            this.SignIn.Text = "Zaloguj";
-            this.SignIn.UseVisualStyleBackColor = true;
-            this.SignIn.Click += new System.EventHandler(this.SignIn_Click);
-            // 
-            // CaptchaBox
-            // 
-            this.CaptchaBox.BackgroundImage = global::Sekretariat.Properties.Resources._1;
-            this.CaptchaBox.Location = new System.Drawing.Point(174, 265);
-            this.CaptchaBox.Name = "CaptchaBox";
-            this.CaptchaBox.Size = new System.Drawing.Size(120, 31);
-            this.CaptchaBox.TabIndex = 15;
-            this.CaptchaBox.TabStop = false;
-            // 
-            // RandomCaptcha
-            // 
-            this.RandomCaptcha.AutoSize = true;
-            this.RandomCaptcha.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.RandomCaptcha.Location = new System.Drawing.Point(317, 300);
-            this.RandomCaptcha.Name = "RandomCaptcha";
-            this.RandomCaptcha.Size = new System.Drawing.Size(62, 15);
-            this.RandomCaptcha.TabIndex = 14;
-            this.RandomCaptcha.Text = "losuj nowy";
-            // 
-            // InstructionLabel
-            // 
-            this.InstructionLabel.AutoSize = true;
-            this.InstructionLabel.Location = new System.Drawing.Point(248, 226);
-            this.InstructionLabel.Name = "InstructionLabel";
-            this.InstructionLabel.Size = new System.Drawing.Size(354, 15);
-            this.InstructionLabel.TabIndex = 13;
-            this.InstructionLabel.Text = "Wpisz znaki, ktore widzisz na ponizszym obrazku, aby kontynowac";
-            // 
-            // VerificationLabel
-            // 
-            this.VerificationLabel.AutoSize = true;
-            this.VerificationLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.VerificationLabel.Location = new System.Drawing.Point(192, 193);
-            this.VerificationLabel.Name = "VerificationLabel";
-            this.VerificationLabel.Size = new System.Drawing.Size(102, 21);
-            this.VerificationLabel.TabIndex = 12;
-            this.VerificationLabel.Text = "Weryfikacja";
-            // 
-            // PasswordLabel
-            // 
-            this.PasswordLabel.AutoSize = true;
-            this.PasswordLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.PasswordLabel.Location = new System.Drawing.Point(241, 110);
-            this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(53, 21);
-            this.PasswordLabel.TabIndex = 11;
-            this.PasswordLabel.Text = "Haslo";
-            // 
-            // UserLabel
-            // 
-            this.UserLabel.AutoSize = true;
-            this.UserLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.UserLabel.Location = new System.Drawing.Point(193, 70);
-            this.UserLabel.Name = "UserLabel";
-            this.UserLabel.Size = new System.Drawing.Size(101, 21);
-            this.UserLabel.TabIndex = 10;
-            this.UserLabel.Text = "Uzytkownik";
-            // 
-            // Login
-            // 
-            this.Controls.Add(this.CaptchaCheck);
-            this.Controls.Add(this.PasswordInput);
-            this.Controls.Add(this.UserInput);
-            this.Controls.Add(this.SignIn);
-            this.Controls.Add(this.CaptchaBox);
-            this.Controls.Add(this.RandomCaptcha);
-            this.Controls.Add(this.InstructionLabel);
-            this.Controls.Add(this.VerificationLabel);
-            this.Controls.Add(this.PasswordLabel);
-            this.Controls.Add(this.UserLabel);
-            this.Name = "Login";
-            this.Size = new System.Drawing.Size(800, 450);
-            ((System.ComponentModel.ISupportInitialize)(this.CaptchaBox)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
+       
 
         public string captcha = "mxyxw";
         public void RandomImage()
@@ -192,14 +68,14 @@ namespace Sekretariat
 
         private void SignIn_Click(object sender, EventArgs e)
         {
-            string log = "admin";
-            string pwd = "Qwerty1@34";
+            var log = "admin";
+            var pwd = "Qwerty1@34";
             bool login = (log == UserInput.Text && pwd == PasswordInput.Text);
             bool matchCap = (captcha == CaptchaCheck.Text);
 
             if (login && matchCap)
             {
-                UserInput.BackColor = Color.Green;
+                // swap to other user control   
             }
             else
             {
